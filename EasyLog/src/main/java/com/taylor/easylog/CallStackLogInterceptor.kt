89e9ab911af46de6b33c2d5fefae86a315797ae8
wisdomtl <l.tang@neoclub.cn>
2022-03-12 17:31:12 +0800

    Chore: make call stack interceptor pretty

--- EasyLog/src/main/java/com/taylor/easylog/CallStackLogInterceptor.kt
+++ EasyLog/src/main/java/com/taylor/easylog/CallStackLogInterceptor.kt
@@ -2,23 +2,30 @@ package com.taylor.easylog
 
 import java.lang.StringBuilder
 
-class CallStackLogInterceptor(private val deep: Int) : LogInterceptor {
+class CallStackLogInterceptor() : LogInterceptor {
     companion object {
-        private const val HEADER = "-------------------------------\n"
-        private const val FOOTER = "\n-------------------------------"
+        private const val HEADER =
+            "┌──────────────────────────────────────────────────────────────────────────────────────────────────────"
+        private const val FOOTER =
+            "└──────────────────────────────────────────────────────────────────────────────────────────────────────"
+        private const val LEFT_BORDER = '│'
+        private val blackList = listOf(
+            CallStackLogInterceptor::class.java.name,
+            EasyLog::class.java.name,
+            Chain::class.java.name,
+        )
     }
 
     override fun log(priority: Int, tag: String, log: String, chain: Chain) {
-        val callStackLog = StringBuilder()
-            .append(HEADER)
-            .append(log)
-            .append(getCallStack(listOf(
-                CallStackLogInterceptor::class.java.name,
-                EasyLog::class.java.name,
-                Chain::class.java.name,
-            )))
-            .append(FOOTER).toString()
-        chain.proceed(priority, tag, callStackLog)
+        chain.proceed(priority, tag, HEADER)
+        chain.proceed(priority, tag, "$LEFT_BORDER$log")
+        getCallStack(blackList).forEach {
+            val callStack = StringBuilder()
+                .append(LEFT_BORDER)
+                .append("\t${it}").toString()
+            chain.proceed(priority, tag, callStack)
+        }
+        chain.proceed(priority, tag, FOOTER)
     }
 
     override fun enable(): Boolean {
--- EasyLog/src/main/java/com/taylor/easylog/Util.kt
+++ EasyLog/src/main/java/com/taylor/easylog/Util.kt
@@ -3,17 +3,9 @@ package com.taylor.easylog
 /**
  * print method call stack
  */
-fun getCallStack(blackList: List<String>): String {
-    return Thread.currentThread().stackTrace.drop(3).filter { it.className !in blackList }
-        .print { "${it.className}.${it.methodName} (line ${it.lineNumber})" }
+fun getCallStack(blackList: List<String>): List<String> {
+    return Thread.currentThread()
+        .stackTrace.drop(3)
+        .filter { it.className !in blackList }
+        .map { "${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})" }
 }
-
-/**
- * print collection bean in which you interested defined by [map]
- */
-fun <T> Collection<T>.print(map: (T) -> String) =
-    StringBuilder("\n[").also { sb ->
-        this.forEach { e -> sb.append("\n\t${map(e)},") }
-        sb.append("\n]")
-    }.toString()
-
